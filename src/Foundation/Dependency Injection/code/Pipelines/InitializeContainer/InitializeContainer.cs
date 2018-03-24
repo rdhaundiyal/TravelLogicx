3b33c815e1f307f69f902dae6898b1cb5a9354c1
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2018-03-24 10:35:35 +0530

    Commit changes

---
+++ src/Foundation/Dependency Injection/code/Pipelines/InitializeContainer/InitializeContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelLogicx.Foundation.DependencyInjection.Pipelines.InitializeContainer
+{
+    public class InitializeContainer
+    {
+    }
+}
