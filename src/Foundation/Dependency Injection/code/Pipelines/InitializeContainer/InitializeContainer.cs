200787cc148601a24f5d34028a290feeef6329e7
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2018-03-31 20:33:52 +0530

    edit

--- src/Foundation/Dependency Injection/code/Pipelines/InitializeContainer/InitializeContainer.cs
+++ src/Foundation/Dependency Injection/code/Pipelines/InitializeContainer/InitializeContainer.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Sitecore.Pipelines;
 
 namespace TravelLogicx.Foundation.DependencyInjection.Pipelines.InitializeContainer
 {
     public class InitializeContainer
     {
+        public void Process(PipelineArgs args)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register dependencies in controllers
+
+            var assembliesInAppDomain = AppDomain.CurrentDomain.GetAssemblies().ToArray();
+            builder.RegisterControllers(assembliesInAppDomain);
+            // Register dependencies in filter attributes
+            builder.RegisterFilterProvider();
+
+            // Register dependencies in custom views
+            builder.RegisterSource(new ViewRegistrationSource());
+
+
+            builder.RegisterAssemblyModules(assembliesInAppDomain);
+
+
+            var container = builder.Build();
+
+            // Set MVC DI resolver to use our Autofac container
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
     }
 }
